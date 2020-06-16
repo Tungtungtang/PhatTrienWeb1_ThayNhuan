e4baec0f288b29d47444fce49078539c044b4cba
tdc-students <cuongtk@tdc.edu.vn>
2020-06-16 14:00:44 +0700

    module phat triển web 1
    Nộp Module lên Github

---
+++ module-tung/1578/1578-content.php
@@ -0,0 +1,57 @@
+<div class="module-1578">
+	<div class="container module-container">
+		<div class="text-title">
+			CATEGORIES
+		</div>
+		<div class="fashion">
+			<a href="#" class="text-fashion">Fashion</a>
+			<div>
+				<span>
+					<a href="#">ACCESSORIES</a>
+				</span>
+				<span>
+					<a href="#">TRENDS</a>
+				</span>
+				<span>
+					<a href="#">FASHION ADVICE</a>
+				</span>
+			</div>
+		</div>
+		<div class="beauty">
+			<a href="#" class="text-beauty">Beauty</a>
+			<div>
+				<span>
+					<a href="#">SKIN CARE</a>
+				</span>
+				<span>
+					<a href="#">NAILS</a>
+				</span>
+				<span>
+					<a href="#">MAKEUO</a>
+				</span>
+				<span>
+					<a href="#">HAIR</a>
+				</span>
+			</div>
+		</div>
+		<div class="lifestyle">
+			<a href="#" class="text-lifestyle">Lifestyle</a>
+			<div>
+				<span>
+					<a href="#">FOOD</a>
+				</span>
+				<span>
+					<a href="#">FITNESS</a>
+				</span>
+				<span>
+					<a href="#">TRAVEL</a>
+				</span>
+			</div>
+		</div>
+
+		<div class="cart">
+			<a href="#"><span><i class="fa fa-shopping-bag"></i></span>
+				<span>Shop the look</span></a>
+		</div>
+	</div>
+</div>
