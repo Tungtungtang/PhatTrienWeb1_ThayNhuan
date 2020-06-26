903b9cc1e13ff7fbbf4dae24a9ec8713e04009e9
Phan Van Tung <phanvantung13579@gmail.com>
2020-06-26 22:30:52 +0700

    update source code

---
+++ module-ptw1/1568/1568-content.php
@@ -0,0 +1,64 @@
+<div class="module-1583">
+	<div class="container module-container">
+		<div class="row module-row">
+			<div class="col-md-4 left" >
+				<div class="image-left">
+					<img alt="#" src="./images/mob-small-logo.png">
+				</div>
+				<div class="content-left">
+					<p>Lorem ipsum dolor – sit amet, consectetur adipiscing elit. Ut tellus, luctus nec mattis. Curabitur eu massa id ex placerat interdum elit dolor met. </p>
+				</div>
+			</div>
+			<div class="col-md-4 center">
+				<div class="content-center">
+					<p>GET ALL THE LATEST UPDATES</p>
+				</div>
+				<div class="form-center">
+					<form class="form-center" >
+    					<input type="text" name="email" placeholder="Email" style="width: 100%"/>
+					</form>
+					<a class="subcribe-center" href="#">SUBSCRIBE</a>
+					<!-- <button class="subcribe-center"> SUBSCRIBE</button> -->
+				</div>
+			</div>
+			<div class="col-md-4 right">
+				<div class="email1">
+					<p >Sponsorship & gifting </p>
+					<a href="#" class="emailhover1">marketing@dream-theme.com</a>
+				</div>
+				<div class="boder"></div>
+				<div class="email2" style="line-height: 0.5rem"> 
+					<p>General questions & feedback</p>	
+					<a href="#" class="emailhover1">hello@dream-theme.com</a>
+				</div>
+				
+			</div>
+		</div>
+        </div>
+     
+    <footer > 
+    	<div class="row footer-row">
+			<div class="col-md-5 footer-left">
+				<div class="content-left">
+					<p >All right reseved 2020 @seven Fashion Blog </p>
+				</div>
+			</div>
+			
+			<div class="col-md-4 footer-left">
+				<div class="content-left">
+					<p> </p>
+				</div>
+			</div>
+
+			<div class="col-md-3 footer-right" >
+				<div class="row row-right">
+					<div class="col-md-4"> <a href="#">About</a></div>
+					<div class="col-md-4"> <a href="#">About</a></div>
+					<div class="col-md-4"> <a href="#">About</a></div>
+				</div>
+			</div>
+		</div>
+    </footer>
+</div>
+
+
---
+++ module-ptw1/1583/1583-content.php
@@ -0,0 +1,97 @@
+<div class="module-1583">
+    <div class="container">
+        <ul class="nav nav-pills nav-fill">
+            <li class="nav-item">
+                <a class="all" data-toggle="pill" href="#all">ALL</a>
+            </li>
+            <li class="nav-item">
+                <a class="casual" data-toggle="pill" href="#casual">CASUAL</a>
+            </li>
+            <li class="nav-item">
+                <a class="night-out" data-toggle="pill" href="#night-out">NIGHT OUT</a>
+            </li>
+            <li class="nav-item">
+                <a class="office" data-toggle="pill" href="#office">OFFICE</a>
+            </li>
+        </ul>
+        <!-- Tab panes -->
+        <div class="tab-content">
+            <div class="tab-pane container active" id="all">
+                <div class="container">
+                    <div class="flex-container">
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="tab-pane container fade" id="casual">
+                <div class="container">
+                    <div class="flex-container">
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div class="tab-pane container fade" id="night-out">
+                <div class="container">
+                    <div class="flex-container">
+                       <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="tab-pane container fade" id="office">
+                <div class="container">
+                    <div class="flex-container">
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                        <div class="img"> <img class="img-fluid" src="images/flex.png" alt=""></div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
