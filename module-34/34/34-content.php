a7c029f5226ea9698baf0a1605bcca2380ac7660
tdc-students <cuongtk@tdc.edu.vn>
2020-06-26 13:05:23 +0700

    module-34
    cắt module

---
+++ module-34/34/34-content.php
@@ -0,0 +1,113 @@
+<div class="module-34">
+	<div class="container container-34">
+		<div class="logo">
+			<img class="hinh" src="images/logo_01.png" alt="#">
+		</div>
+		<div class="menu-34">
+			<nav class="navbar menu">
+				<div class="container-fluid">
+					<ul class="nav navbar-nav">
+						<li class="dropdown">
+							<a class="home" href="#">Home
+								<i class="fa fa-angle-right right" aria-hidden="true"></i>
+								<i class="fa fa-angle-down down" aria-hidden="true"></i>
+							</a>
+							<div class="dropdown-content">
+								<a class="dropdown-item" href="#">Tea Production</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#">World Tea Company</a>
+							</div>
+						</li>
+						<li class="dropdown">
+							<a href="#">About us
+								<i class="fa fa-angle-right right" aria-hidden="true"></i>
+								<i class="fa fa-angle-down down" aria-hidden="true"></i>
+							</a>
+							<div class="dropdown-content">
+								<a class="dropdown-item" href="#">About Company</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#">Testimonials</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#">Team</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#">FAQ</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#">404 Page</a>
+							</div>
+						</li>
+						<li class="dropdown">
+							<a href="#">Products
+								<i class="fa fa-angle-right right" aria-hidden="true"></i>
+								<i class="fa fa-angle-down down" aria-hidden="true"></i>
+							</a>
+							<div class="dropdown-content">
+								<a class="dropdown-item" href="#">Tea Production</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#">World Tea Company</a>
+							</div>
+						</li>
+						<li class="dropdown">
+							<a href="#">Blog
+								<i class="fa fa-angle-right right" aria-hidden="true"></i>
+								<i class="fa fa-angle-down down" aria-hidden="true"></i>
+							</a>
+							<div class="dropdown-content">
+								<a class="dropdown-item" href="#">Tea Production</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#">World Tea Company</a>
+							</div>
+						</li>
+						<li class="dropdown">
+							<a href="#">Gallery
+								<i class="fa fa-angle-right right" aria-hidden="true"></i>
+								<i class="fa fa-angle-down down" aria-hidden="true"></i>
+							</a>
+							<div class="dropdown-content">
+								<a class="dropdown-item" href="#">Tea Production</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#">World Tea Company</a>
+							</div>
+						</li>
+						<li>
+							<a href="#">Contacts</a>
+						</li>
+						<li class="dropdown">
+							<a href="#">Pages
+							</a>
+							<div class="dropdown-content">
+								<a class="dropdown-item" href="#">Tea Production</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#">World Tea Company</a>
+							</div>
+						</li>
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li class="icon-nav">
+							<a class="icon" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+						</li>
+						<li>
+							<a href="#" class="icon">
+								<i class="fa fa-shopping-bag" aria-hidden="true">
+								</i>
+								<span> 0</span>
+							</a>
+						</li>
+						<li>
+							<a href="#" class="icon"> <i class="fa fa-user" aria-hidden="true"></i> Login </a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<div class="contents-title">
+				<h1 class="content1">Quality tea production </h1>
+				<h1 class="content2">
+					from India
+				</h1>
+				<h4 class="content3">Integer quis tempor orci. Suspendisse potenti. Interdum et malesuada fames ac
+
+				</h4>
+				<h4 class="content3">ante ipsum primis in faucibus.</h4>
+			</div>
+		</div>
+	</div>
+</div>
